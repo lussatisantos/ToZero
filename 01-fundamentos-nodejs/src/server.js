d195c44398735d8dee3d066af0cff07aa17f72d7
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2024-02-28 13:59:19 +0000

    Initial commit

---
+++ 01-fundamentos-nodejs/src/server.js
@@ -0,0 +1,32 @@
+import http from 'node:http'
+import { json } from './mifddleware/json.js'
+
+const users = []
+
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req
+
+    await json(req, res)
+
+    if(method == 'GET' && url == '/users'){
+        return res
+            .setHeader('Content-Type', 'application/json')
+            .end(JSON.stringify(users))
+    }
+
+    if(method == 'POST' && url == '/users'){
+        const { name, email } = req.body
+
+        users.push({
+            id: 1,
+            name,
+            email,
+        })
+
+        return res.writeHead(201).end()
+    }
+
+    return res.writeHead(404).end()
+})
+
+server.listen(3333)
